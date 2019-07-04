db5cd0b089d6040d4bc557a80b6bf99a6667f136
Brad <powelbra@oregonstate.edu>
2019-07-03 23:05:18 -0700

    playMinion
    Moved minion to its own function.

--- projects/powelbra/dominion/cardEffect.c
+++ projects/powelbra/dominion/cardEffect.c
@@ -1,5 +1,6 @@
 #include "cardEffect.h"
 #include "dominion.h"
+#include "dominion_helpers.h"
 #include <stdio.h>
 #include "rngs.h"
 #include <stdlib.h>
@@ -16,7 +17,7 @@ void playBaron(int choice, struct gameState *state, int handPos) {
 				state->coins += 4;//Add 4 coins to the amount of coins
 				state->discard[currentPlayer][state->discardCount[currentPlayer]] = state->hand[currentPlayer][p];	// Add to the discard pile
 				state->discardCount[currentPlayer]++;
-				for (; p < state->handCount[currentPlayer]; p++) {
+				for (; p < state->handCount[currentPlayer]; p++) {	// Starts from current position in hand and continues
 					state->hand[currentPlayer][p] = state->hand[currentPlayer][p + 1];
 				}
 				state->hand[currentPlayer][state->handCount[currentPlayer]] = -1;
@@ -47,3 +48,43 @@ void playBaron(int choice, struct gameState *state, int handPos) {
 
 }
 
+void playMinion(int choice1, int choice2, struct gameState *state, int handPos) {
+	int currentPlayer = whoseTurn(state);
+	//+1 action
+	state->numActions++;
+
+	if (choice1) {		//+2 coins
+		state->coins = state->coins + 2;
+		//discard card from hand
+		discardCard(handPos, currentPlayer, state, 0);
+	}
+
+	else if (choice2) {		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
+		//discard hand ********* May need to be changed to a manual discard since this function puts cards into the played area ***********
+		while (numHandCards(state) > 0) {
+			discardCard(handPos, currentPlayer, state, 0);
+		}
+
+		//draw 4
+		for (i = 0; i < 4; i++) {
+			drawCard(currentPlayer, state);
+		}
+
+		//other players discard hand and redraw if hand size > 4
+		for (i = 0; i < state->numPlayers; i++) {
+			if (i != currentPlayer) {
+				if (state->handCount[i] > 4) {
+					//discard hand
+					while (state->handCount[i] > 0) {
+						discardCard(handPos, i, state, 0);
+					}
+
+					//draw 4
+					for (j = 0; j < 4; j++) {
+						drawCard(i, state);
+					}
+				}
+			}
+		}
+	}
+}
--- projects/powelbra/dominion/cardEffect.h
+++ projects/powelbra/dominion/cardEffect.h
@@ -1,9 +1,14 @@
 #ifndef _CARDEFFECT_H
 #define _CARDEFFECT_H
 
+#include "dominion.h"
+
+/* Function to handle the Baron card. 'Choice' is the decision to discard an estate (1)
+or gain an estate (0). Also needs the game state and the card's position in hand. */
 void playBaron(int choice, struct gameState *state, int handPos);
 
-
+/* Choice 1 is > 0 to gain coins while Choice 2 is > 0 for redraw.*/
+void playMinion(int choice1, int choice2, struct gameState *state, int handPos);
 
 
 
