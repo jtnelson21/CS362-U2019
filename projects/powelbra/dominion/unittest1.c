13563f330f32b925cb18acf34761f61cea3d4ffc
Brad <powelbra@oregonstate.edu>
2019-07-12 14:44:35 -0700

    Update unittest1.c
    Made assertions more informative.

--- projects/powelbra/dominion/unittest1.c
+++ projects/powelbra/dominion/unittest1.c
@@ -24,6 +24,7 @@ int kingdomNoChange(struct gameState *G, struct gameState *testG) {
 	// Check all supply piles and verify no change has occured
 	for (i = 0; i <= treasure_map; i++) {
 		if (G->supplyCount[i] != testG->supplyCount[i]) {
+			printf("Supply %d changed.\n", i)
 			return -1;
 		}
 	}
@@ -34,33 +35,45 @@ int kingdomNoChange(struct gameState *G, struct gameState *testG) {
 // Verify the players were unchanged
 // Returns -1 on fail, 1 on success
 int oppNoChange(struct gameState *G, struct gameState *testG) {
-	int i, j;
+	int i, j, x = 1;	// x is returned; set to success by default
 	for (i = 1; i < testG->numPlayers; i++) {
 		// Verify hand, deck, and discard counts have changed.
-		if (G->handCount[i] != testG->handCount[i] || G->deckCount[i] != testG->deckCount[i] || G->discardCount[i] != testG->discardCount[i]) {
-			return -1;
+		if (G->handCount[i] != testG->handCount[i]) {
+			printf("Player %d's handCount changed.\n", i);
+			x = -1;
+		}
+		if (G->deckCount[i] != testG->deckCount[i]) {
+			printf("Player %d's deckCount changed.\n", i);
+			x = -1;
+		}
+		if (G->discardCount[i] != testG->discardCount[i]) {
+			printf("Player %d's discardCount changed.\n", i);
+			x = -1;
 		}
 		// Verify cards in hand are same
 		for (j = 0; j < testG->handCount[i]; j++) {
 			if (G->hand[i][j] != testG->hand[i][j]) {
-				return -1;
+				printf("Player %d's hand change at position %d\n", i, j);
+				x = -1;
 			}
 		}
 		// Verify cards in deck are same
 		for (j = 0; j < testG->deckCount[i]; j++) {
 			if (G->deck[i][j] != testG->deck[i][j]) {
-				return -1;
+				printf("Player %d's deck change at position %d\n", i, j);
+				x = -1;
 			}
 		}
 		// Verify cards in discard are same
 		for (j = 0; j < testG->discardCount[i]; j++) {
 			if (G->discard[i][j] != testG->discard[i][j]) {
-				return -1;
+				printf("Player %d's discard change at position %d\n", i, j);
+				x = -1;
 			}
 		}
 	}
 	// If loop ends, no change detected
-	return 1;
+	return x;
 }
 
 // Verify hand remains unchanged; Assumes played card was last in hand
@@ -70,11 +83,14 @@ int handNoChange(struct gameState *G, struct gameState *testG) {
 	int i;
 
 	if (G->handCount[currentPlayer] != testG->handCount[currentPlayer]) {
+		printf("handCount changed.\n");
 		return -1;
 	}
 	else {
 		for (i = 0; i < G->handCount[currentPlayer]; i++) {
 			if (G->hand[currentPlayer][i] != testG->hand[currentPlayer][i]) {
+				printf("Card at position %d changed.\n", i);
+				printf("Original: %d\tNew: %d\n", G->hand[currentPlayer][i], testG->hand[currentPlayer][i]);
 				return -1;
 			}
 		}
@@ -90,11 +106,14 @@ int deckNoChange(struct gameState *G, struct gameState *testG) {
 	int i;
 
 	if (G->deckCount[currentPlayer] != testG->deckCount[currentPlayer]) {
+		printf("deckCount changed.\n");
 		return -1;
 	}
 	else {
 		for (i = 0; i < G->deckCount[currentPlayer]; i++) {
 			if (G->deck[currentPlayer][i] != testG->deck[currentPlayer][i]) {
+				printf("Card at position %d changed.\n", i);
+				printf("Original: %d\tNew: %d\n", G->deck[currentPlayer][i], testG->deck[currentPlayer][i]);
 				return -1;
 			}
 		}
