cdbfdcbc42bb34cd9bef3709ed2d71122101a639
Brad <powelbra@oregonstate.edu>
2019-07-12 21:39:45 -0700

    Tests in function

--- projects/powelbra/dominion/unittest5.c
+++ projects/powelbra/dominion/unittest5.c
@@ -18,29 +18,19 @@ unittest1: unittest5.c dominion.o rngs.o cardEffect.o testHelp.o
 
 #define TESTCARD "Mine"
 
-int main() {
+void testMine(int trash, int target, int expect, struct gameState *G) {
 	int i, currentPlayer, errTest;
 	int handPos = 0, choice1 = 0, choice2 = 0, choice3 = 0;
-
-	// Initialize the game
-	int seed = 100;
-	int numPlayers = 2;
-	struct gameState G, testG;
-	int k[10] = { baron, minion, ambassador, tribute, mine, gardens, village, smithy, adventurer, great_hall };
-
-	printf("--------------- Testing Card: %s ---------------\n", TESTCARD);
-
-	// ---- Test 1: copper -> silver -----
-	printf("\n----- TEST 1: copper -> silver -----\nDEBUG statements:\n");
+	struct gameState testG;
 
 	// Set up game
-	initializeGame(numPlayers, k, seed, &G);
-	currentPlayer = whoseTurn(&G);
-	G.hand[currentPlayer][0] = copper;	// make sure there's a treasure to use
+	initializeGame(numPlayers, k, seed, G);
+	currentPlayer = whoseTurn(G);
+	G->hand[currentPlayer][0] = trash;	// make sure there's a treasure to use
 	choice1 = 0;
-	choice2 = silver;
-	updateCoins(currentPlayer, &G, 0);
-	memcpy(&testG, &G, sizeof(struct gameState));
+	choice2 = target;
+	updateCoins(currentPlayer, G, 0);
+	memcpy(&testG, G, sizeof(struct gameState));
 
 	// Add mine to hand
 	testG.hand[currentPlayer][testG.handCount[currentPlayer]] = mine;
@@ -54,28 +44,55 @@ int main() {
 	printf("Should be 0: %d\n", errTest);
 	printf("Expected buys: 1\t\tActual buys: %d\n", testG.numBuys);
 	printf("Expected actions: 0\t\tActual actions: %d\n", testG.numActions);
-	printf("Expected coin value: +1\t\tActual coins: +%d\n", testG.coins - G.coins);
+	printf("Expected coin value: %d\t\tActual coins: %d\n", expect, testG.coins - G->coins);
 	printf("Expected handCount: 5\t\tActual handCount: %d\n", testG.handCount[currentPlayer]);
 
-	if (deckCheck(currentPlayer, &G, &testG) == 1) {
+	if (deckCheck(currentPlayer, G, &testG) == 1) {
 		printf("Deck ok.\t");
 	}
-	if (discardCheck(currentPlayer, &G, &testG) == 1) {
+	if (discardCheck(currentPlayer, G, &testG) == 1) {
 		printf("Discard ok.\t");
 	}
-	if (oppNoChange(&G, &testG) == 1) {
+	if (oppNoChange(G, &testG) == 1) {
 		printf("Opponents ok.\n");
 	}
-	
+
 	// Check only the target treasure changes
-	printf("Supply pile %d should change: ", silver);
-	if (kingdomNoChange(&G, &testG) != 1) {
+	printf("Supply pile %d should change: ", choice2);
+	if (kingdomNoChange(G, &testG) != -1) {
 		printf("Supply didn't change!\n");
 	}
 	else {
-		printf("\n");
-	}
-	// 	printf("Supply pile %d, Expected: %d\tActual: %d\n", silver, G.supplyCount[silver] - 1, testG.supplyCount[silver]);
+		printf("\nExpected: %d\tActual: %d\n", choice2, G->supplyCount[choice2] - 1, testG.supplyCount[choice2]);
+
+
+}
+
+
+int main() {
+	// Initialize the game
+	int seed = 100;
+	int numPlayers = 2;
+	struct gameState G;
+	int k[10] = { baron, minion, ambassador, tribute, mine, gardens, village, smithy, adventurer, great_hall };
+
+	printf("--------------- Testing Card: %s ---------------\n", TESTCARD);
+
+	// Run tests
+	printf("\n----- TEST 1: copper -> copper -----\nDEBUG statements:\n");
+	testMine(copper, copper, 0, &G);
+	printf("\n----- TEST 2: copper -> silver -----\nDEBUG statements:\n");
+	testMine(copper, silver, 1, &G);
+	printf("\n----- TEST 3: silver -> silver -----\nDEBUG statements:\n");
+	testMine(silver, silver, 0, &G);
+	printf("\n----- TEST 4: silver -> gold -----\nDEBUG statements:\n");
+	testMine(silver, gold, 1, &G);
+	printf("\n----- TEST 5: gold -> copper -----\nDEBUG statements:\n");
+	testMine(gold, copper, -2, &G);
+	printf("\n----- TEST 6: gold -> silver -----\nDEBUG statements:\n");
+	testMine(gold, silver, -1, &G);
+	printf("\n----- TEST 7: gold -> gold -----\nDEBUG statements:\n");
+	testMine(gold, gold, 0, &G);
 
 
 
