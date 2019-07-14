90c8263f0d0e93b97ca7223dec0060756e769d22
Brad <powelbra@oregonstate.edu>
2019-07-13 22:31:42 -0700

    Update cardtest5.c

--- projects/powelbra/dominion/cardtest5.c
+++ projects/powelbra/dominion/cardtest5.c
@@ -20,14 +20,14 @@ int main() {
 	int numPlayers = 2, i, seed = 10;
 	int k[10] = { baron, minion, ambassador, tribute, mine, gardens, village, smithy, adventurer, great_hall };
 	struct gameState G;
-	int topCard, errTest, prevHandC, prevDeckC;
+	int topCard, errTest, prevHandC, prevDeckC, shuffleCheck;
 
 
 	initializeGame(numPlayers, k, seed, &G);
 
 
 	printf("---------- Testing Function: %s ----------\n", TESTFUNCTION);
-	printf("Testing 30 draws on Deck = 15, Discard = 10.\n");
+	printf("---Testing 30 draws on Deck = 15, Discard = 10 ---\n");
 
 	G.deckCount[0] = 15;
 	for (i = 0; i < G.deckCount[0]; i++) {
@@ -38,15 +38,24 @@ int main() {
 		G.discard[0][i] = i + 15;	// Baron, great hall... salvager
 	}
 
+
 	for (i = 0; i < 30; i++) {
-		printf("Draw %d\n", i + 1);
 		topCard = G.deck[0][G.deckCount[0] - 1];	// Set topCard to top card of the deck
 		prevHandC = G.handCount[0];	// Store handcount
 		prevDeckC = G.deckCount[0];	// Store deckcount
+		if (prevDeckC == 0) {	// Store discard count when shuffling
+			shuffleCheck = G.discardCount[0];
+		}
 		errTest = drawCard(0, &G);
-		printf("Returned value: %d\n", errTest);
+		printf("Draw %d return: %d\n", i + 1, errTest);
 		if (prevDeckC == 0) {
 			printf("Shuffling...\n");
+			if (G.deckCount[0] != shuffleCheck - 1) {	// shuffleCheck - 1 because a card was drawn
+				printf("Not all of the cards were shuffled!\n");
+			}
+			if (G.discardCount[0] != 0) {
+				printf("Discard count off.\n");
+			}
 		}
 		else {
 			if (G.hand[0][G.handCount[0] - 1] != topCard) {
@@ -59,11 +68,68 @@ int main() {
 				printf("Deck count off by %d\n", G.deckCount[0] - (prevHandC - 1));
 			}
 		}
-
-
 	}
 
+	// Test played area
+	printf("--- Testing if cards in played area stay put (they should!) ---\n");
+	printf("5 cards in deck, 3 in discard. 10 Draws.")
+	initializeGame(numPlayers, k, seed, &G);
+	G.discardCount[0] = 3;
+	for (i = 0; i < G.discardCount[0]; i++) {
+		G.discard[0][i] = minion;
+	}
+	G.playedCardCount = 5;
+	for (i = 0; i < G.playedCardCount; i++) {
+		G.playedCards[i] = baron;
+	}
 
+	for (i = 0; i < 10; i++) {
+		topCard = G.deck[0][G.deckCount[0] - 1];	// Set topCard to top card of the deck
+		prevHandC = G.handCount[0];	// Store handcount
+		prevDeckC = G.deckCount[0];	// Store deckcount
+		if (prevDeckC == 0) {	// Store discard count when shuffling
+			shuffleCheck = G.discardCount[0];
+		}
+		errTest = drawCard(0, &G);
+		printf("Draw %d return: %d\n", i + 1, errTest);
+		if (prevDeckC == 0) {
+			printf("Shuffling...\n");
+			if (G.deckCount[0] != shuffleCheck - 1) {	// shuffleCheck - 1 because a card was drawn
+				printf("Not all of the cards were shuffled!\n");
+			}
+			if (G.discardCount[0] != 0) {
+				printf("Discard count off.\n");
+			}
+		}
+		else {
+			if (G.hand[0][G.handCount[0] - 1] != topCard) {
+				printf("Drawn card is different from what was the top card.\n");
+			}
+			if (G.handCount[0] != prevHandC + 1) {
+				printf("Hand count off by %d\n", G.handCount[0] - (prevHandC + 1));
+			}
+			if (G.deckCount[0] != prevDeckC - 1) {
+				printf("Deck count off by %d\n", G.deckCount[0] - (prevHandC - 1));
+			}
+		}
+	}
+
+	int sentinel = 1;
+	if (G.playedCardCount != 5) {
+		printf("playedCardCount off by %d\n", G.playedCardCount - 5);
+		sentinel = 0;
+	}
+	else {
+		for (i = 0; i < 5, i++) {
+			if (G.playedCards[i] != baron) {
+				printf("Played card at %d changed to %d.\n", i, G.playedCards[i]);
+				sentinel = 0;
+			}
+		}
+	}
+	if (sentinel) {
+		printf("Played card area unaffected!\n");
+	}
 
 	return 0;
 }
